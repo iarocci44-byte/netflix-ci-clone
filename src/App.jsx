63f96cd1ddb973d0f4188f5b3204fa07d414e904
iarocci44-byte <iarocci44@gmail.com>
2025-12-01 13:13:29 -0500

    testing vercel

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Reasons from "./component/Reasons";
 import Trends from "./component/Trends";
 import Footer from "./component/Footer";
 import Subscription from "./component/Singerup"
-//import './App.css'  adding to this comment to make a change
+//import './App.css'
 
 
 const App = () => {
   return(
     <>
-      <div className="bg-black min-h-screen px-12 text-white">
+      <div className="bg-black min-h-screen px-12 text-white scroll-smooth">
         <Nav />
         <Trends />
         <Reasons />
--- src/component/Trends.jsx
+++ src/component/Trends.jsx
@@ -4,12 +4,13 @@ import trend2 from "../assets/Frankenstein.webp";
 import trend3 from "../assets/MonsterEdGein.webp";
 import trend4 from "../assets/TheWitcher.webp";
 import trend5 from "../assets/Wednesday.webp";
+import trend6 from "../assets/KPOP.webp";
+import trend7 from "../assets/NobodyWantsThis.webp";
+import trend8 from "../assets/TheBeastInMe.webp";
 import { PiArrowSquareLeft } from "react-icons/pi";
 import { PiArrowSquareRight } from "react-icons/pi";
 
-
 function Trends() {
-  
   const movies = [
     {
       id: 1,
@@ -33,56 +34,58 @@ function Trends() {
     },
     {
       id: 6,
-      url: trend5,
+      url: trend6,
     },
     {
       id: 7,
-      url: trend5,
+      url: trend7,
     },
     {
       id: 8,
-      url: trend5,
+      url: trend8,
     },
   ];
   const rowRef = useRef(null);
   const handleScrollRight = () => {
-  rowRef.current.scrollLeft += 300; // moves right
-};
+    rowRef.current.scrollLeft += 300; // moves right
+  };
 
-const handleScrollLeft = () => {
-  rowRef.current.scrollLeft -= 300; // moves left
-};
-  
+  const handleScrollLeft = () => {
+    rowRef.current.scrollLeft -= 300; // moves left
+  };
+
   return (
     <div className="pt-5 h-auto">
-        
-        
       <div className="font-bold text-2xl pb-5 h-auto">Trending Now</div>
       <div className="flex">
-      <button onClick={handleScrollLeft} className="text-8xl cursor-pointer"><PiArrowSquareLeft /></button>
-      <div ref={rowRef} className="relative pl-4 flex gap-10 max-w-290 overflow-scroll hide-scrollbar overflow-y-auto h-[340px]">
-        {movies.map((movie, index) => {
-          return (
-            <div key={index} className="relative h-fit pt-7">
-              
-              <img
-                className="hover:scale-110 transition-transform duration-300 rounded-2xl min-w-[200px] h-auto"
-                src={movie.url}
-                alt=""
-              />
-              <div className="text-8xl font-bold absolute bottom-0 left-[-25px] text-stroke-white h-auto">
-                {movie.id}
+        <button onClick={handleScrollLeft} className="text-8xl cursor-pointer">
+          <PiArrowSquareLeft />
+        </button>
+        <div
+          ref={rowRef}
+          className="relative pl-4 flex gap-10 max-w-290 overflow-scroll hide-scrollbar overflow-y-auto h-[340px]"
+        >
+          {movies.map((movie, index) => {
+            return (
+              <div key={index} className="relative h-fit pt-7">
+                <img
+                  className="hover:scale-110 transition-transform duration-300 rounded-2xl min-w-[200px] h-auto"
+                  src={movie.url}
+                  alt=""
+                />
+                <div className="text-8xl font-bold absolute bottom-0 left-[-25px] text-stroke-white h-auto">
+                  {movie.id}
+                </div>
               </div>
-              
-            </div>
-          );
-        })}
-      </div>
-      <button onClick={handleScrollRight} className="text-8xl cursor-pointer"><PiArrowSquareRight /></button>
+            );
+          })}
+        </div>
+        <button onHover={handleScrollRight} className="text-8xl cursor-pointer">
+          <PiArrowSquareRight />
+        </button>
       </div>
     </div>
   );
-
 }
 
 export default Trends;
