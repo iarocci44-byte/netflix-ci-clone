27f296a74304952f31b790e7ceea98b52a9ea465
iarocci44-byte <iarocci44@gmail.com>
2025-11-30 21:02:42 -0500

    new repository

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import FrequentlyAsked from "./component/FrequentlyAsked";
+import Nav from "./component/Nav";
+import Reasons from "./component/Reasons";
+import Trends from "./component/Trends";
+import Footer from "./component/Footer";
+import Subscription from "./component/Singerup"
+//import './App.css'
+
+
+const App = () => {
+  return(
+    <>
+      <div className="bg-black min-h-screen px-12 text-white">
+        <Nav />
+        <Trends />
+        <Reasons />
+        <FrequentlyAsked />
+        <Subscription />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App;
